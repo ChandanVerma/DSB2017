4110f9a03efe7b319b99710eca8cc1e318d8b902
Chandan Verma <verma.chandan01@gmail.com>
2017-08-17 14:13:57 +0530

    Create Tensorflow_CNN.py

---
+++ Tensorflow_CNN.py
@@ -0,0 +1,286 @@
+# A simple CNN to predict certain characteristics of the human subject from MRI images.
+# 3d convolution is used in each layer.
+# Reference: https://www.tensorflow.org/get_started/mnist/pros, http://blog.naver.com/kjpark79/220783765651
+# Adjust needed for your dataset e.g., max pooling, convolution parameters, training_step, batch size, etc
+
+width = 64
+height = 64
+depth = 20
+nLabel = 2
+
+# Start TensorFlow InteractiveSession
+#import input_3Dimage
+import tensorflow as tf
+import os
+import numpy as np
+import pandas as pd
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+sess = tf.InteractiveSession()
+
+train_ids = [id.replace(".npy", "") for id in os.listdir('C:/Unet/resized/train/')]
+valid_ids = [id.replace(".npy", "") for id in os.listdir('C:/Unet/resized/valid/')]
+test_ids = [id.replace(".npy", "") for id in os.listdir('C:/Unet/resized/test/')]
+
+train_ids.sort()
+valid_ids.sort()
+test_ids.sort()
+
+df = pd.read_csv('D:/Projects/Kaggle/DataScienceBowl/stage1_labels.csv')
+
+df.head()
+#%%
+
+def save_array(path, arr):
+    np.save(path, arr)
+    
+
+def load_array(path):
+    return np.load(path)
+	
+train_labels = df["cancer"][df["id"].isin(train_ids)].values
+valid_labels = df["cancer"][df["id"].isin(valid_ids)].values
+#%%
+ Placeholders (MNIST image:28x28pixels=784, label=10)
+x = tf.placeholder(tf.float32, shape=[None, width*height*depth]) # [None, 28*28]
+y_ = tf.placeholder(tf.float32, shape=[None, nLabel])  # [None, 10]
+
+## Weight Initialization
+# Create lots of weights and biases & Initialize with a small positive number as we will use ReLU
+
+#%%
+
+## First Convolutional Layer
+# Conv then Max-pooling. 1st layer will have 32 features for each 5x5 patch. (1 feature -> 32 features)
+W_conv1 = weight_variable([5, 5, 5, 1, 32])  # shape of weight tensor = [5,5,1,32]
+b_conv1 = bias_variable([32])  # bias vector for each output channel. = [32]
+
+# Reshape 'x' to a 4D tensor (2nd dim=image width, 3rd dim=image height, 4th dim=nColorChannel)
+x_image = tf.reshape(x, [-1,width,height,depth,1]) # [-1,28,28,1]
+print(x_image.get_shape) # (?, 64, 64, 20, 1)  # -> output image: 28x28 x1
+
+# x_image * weight tensor + bias -> apply ReLU -> apply max-pool
+h_conv1 = tf.nn.relu(conv3d(x_image, W_conv1) + b_conv1)  # conv2d, ReLU(x_image * weight + bias)
+print(h_conv1.get_shape) # (?, 64, 64, 20, 32)  # -> output image: 28x28 x32
+h_pool1 = max_pool_2x2(h_conv1)  # apply max-pool 
+print(h_pool1.get_shape) # (?, 32, 32, 20, 32)  # -> output image: 14x14 x32
+
+
+## Second Convolutional Layer
+# Conv then Max-pooling. 2nd layer will have 64 features for each 5x5 patch. (32 features -> 64 features)
+W_conv2 = weight_variable([5, 5, 5, 32, 64]) # [5, 5, 32, 64]
+b_conv2 = bias_variable([64]) # [64]
+
+h_conv2 = tf.nn.relu(conv3d(h_pool1, W_conv2) + b_conv2)  # conv2d, .ReLU(x_image * weight + bias)
+print(h_conv2.get_shape) # (?, 32, 32, 20, 64)  # -> output image: 14x14 x64
+h_pool2 = max_pool_2x2(h_conv2)  # apply max-pool 
+print(h_pool2.get_shape) # (?, 64, 64, 10, 64)    # -> output image: 7x7 x64
+
+
+## Densely Connected Layer (or fully-connected layer)
+# fully-connected layer with 1024 neurons to process on the entire image
+W_fc1 = weight_variable([16*16*3*64, 1024])  # [7*7*64, 1024]
+b_fc1 = bias_variable([1024]) # [1024]]
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 16*16*3*64])  # -> output image: [-1, 7*7*64] = 3136
+print(h_pool2_flat.get_shape)  # (?, 2621420)
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)  # ReLU(h_pool2_flat x weight + bias)
+print(h_fc1.get_shape) # (?, 1024)  # -> output: 1024
+
+## Dropout (to reduce overfitting; useful when training very large neural network)
+# We will turn on dropout during training & turn off during testing
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+print(h_fc1_drop.get_shape)  # -> output: 1024
+
+## Readout Layer
+W_fc2 = weight_variable([1024, nLabel]) # [1024, 10]
+b_fc2 = bias_variable([nLabel]) # [10]
+
+y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+print(y_conv.get_shape)  # -> output: 10
+
+## Train and Evaluate the Model
+# set up for optimization (optimizer:ADAM)
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)  # 1e-4
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess.run(tf.global_variables_initializer())
+#%%
+def get_train_batches():
+
+    for ix, patient in enumerate(train_ids):
+        sample = load_array("C:/Unet/resized/train/{}.npy".format(patient))
+        #sample = np.array([imresize(toimage(im), size=(IMG_SIZE_PX, IMG_SIZE_PX)) for im in sample])
+        
+        try:
+            #sample = np.array([sample[bottom:top, :, :].reshape(1, SLICE_COUNT, IMG_SIZE_PX, IMG_SIZE_PX)])
+            label = np.array([train_labels[ix]])
+            if label == 1: label=np.array([0,1])
+            elif label == 0: label=np.array([1,0])
+            return sample, label
+            
+        except Exception as e:
+            print(patient, e, sample.shape)
+            continue
+
+            
+def get_valid_batches():
+    for ix, patient in enumerate(valid_ids):
+        sample = load_array("C:/Unet/resized/valid/{}.npy".format(patient))
+        
+        try:
+            #sample = np.array([sample[bottom:top, :, :].reshape(1, SLICE_COUNT, IMG_SIZE_PX, IMG_SIZE_PX)])
+            label = np.array([valid_labels[ix]])
+            if label == 1: label=np.array([0,1])
+            elif label == 0: label=np.array([1,0])
+            return sample, label
+            
+        except Exception as e:
+            print(patient, e, sample.shape)
+            continue
+            
+#%%   
+trn_gen = get_train_batches()
+trn_gen = np.array(trn_gen)
+
+print(trn_gen[0].shape)
+ 
+
+train_label = load_array('C:/Unet/resized/train_label.npy')
+
+valid_label = load_array('C:/Unet/resized/valid_label.npy')
+
+test_label = load_array('C:/Unet/resized/test_label.npy')
+        
+train_data = load_array('C:/Unet/resized/train_data.npy')
+print(train_data.shape,train_label.shape)
+
+valid_data = load_array('C:/Unet/resized/valid_data.npy')
+print(valid_data.shape,valid_label.shape)
+
+test_data = load_array('C:/Unet/resized/test_data.npy')
+print(test_data.shape,test_label.shape)
+#%%
+
+depth = 20
+image_size = 64
+num_labels = 2
+num_channels = 1 # grayscale
+batch_size = 16
+import numpy as np
+
+def reformat(dataset, labels):
+  dataset = dataset.reshape(
+    (-1,depth, image_size, image_size, num_channels)).astype(np.float32)
+  labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
+  return dataset, labels
+train_dataset, train_labels = reformat(train_data, train_label)
+valid_dataset, valid_labels = reformat(valid_data, valid_label)
+test_dataset, test_labels = reformat(test_data, test_label)
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
+print('Test set', test_dataset.shape, test_labels.shape)
+#%%
+def accuracy(predictions, labels):
+  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+          / predictions.shape[0])
+
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+## Convolution and Pooling
+# Convolution here: stride=1, zero-padded -> output size = input size
+def conv3d(x, W):
+  return tf.nn.conv3d(x, W, strides=[1, 1, 1, 1, 1], padding='SAME') # conv2d, [1, 1, 1, 1]
+
+# Pooling: max pooling over 2x2 blocks
+def max_pool_2x2(x):  # tf.nn.max_pool. ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1]
+  return tf.nn.max_pool3d(x, ksize=[1, 2, 2, 2, 1], strides=[1, 2, 2, 2, 1], padding='SAME')
+#%%
+graph = tf.Graph()
+
+with graph.as_default():
+
+  # Input data.
+  tf_train_dataset = tf.placeholder(tf.float32, shape=[None, width*height*depth])
+  print(tf_train_dataset)
+  tf_train_labels = tf.placeholder(tf.float32, shape=(None, num_labels))
+  print(tf_train_labels)
+  tf_valid_dataset = tf.constant(valid_dataset)
+  print(tf_valid_dataset)
+  tf_test_dataset = tf.constant(test_dataset)
+  print(tf_test_dataset)
+  # Variables.
+  layer1_weights = weight_variable([5, 5, 5, 1, 32]) 
+  print(layer1_weights)
+  layer1_biases = bias_variable([32]) 
+  print(layer1_biases)
+  layer2_weights =  weight_variable([5, 5, 5, 32, 64])
+  print(layer2_weights)
+  layer2_biases = bias_variable([64])
+  print(layer2_biases)
+  fc1_layer_weights = weight_variable([16*16*5*64, 1024])
+  print(fc1_layer_weights)
+  fc1_layer_biases = bias_variable([1024])
+  print(fc1_layer_biases)
+  fc2_layer_weights = weight_variable([1024, nLabel])
+  print(fc2_layer_weights)
+  fc2_layer_biases = bias_variable([nLabel])
+  print(fc2_layer_biases)
+#%%
+def model(data):
+    conv1 = tf.nn.relu(conv3d(data, layer1_weights) + layer1_biases)
+    print('Conv1 layer shape is:' ,conv1.shape)
+    pool1 = max_pool_2x2(conv1)
+    print('Pooling layer 1 shape is:' ,pool1.shape)
+    conv2 = tf.nn.relu(conv3d(pool1, layer2_weights) + layer2_biases)
+    print('Conv2 layer shape is:' ,conv2.shape)
+    pool2 = max_pool_2x2(conv2)
+    print('Pooling layer 2 shape is:' ,type(pool2.shape))
+    #shape = pool2.get_shape().as_list()
+    shape = tf.convert_to_tensor([np.nan, 16, 16, 5, 64])
+    reshape = tf.reshape(pool2, [-1, shape[1] * shape[2] * shape[3] * shape[4]])
+    print(reshape.shape)
+    fc1 = tf.nn.relu(tf.matmul(reshape, fc1_layer_weights) + fc1_layer_biases)
+    return tf.matmul(fc1, fc2_layer_weights) + fc2_layer_biases
+#%%  
+  # Training computation.
+logits = model(tf_train_dataset)
+#loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+#correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+#accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))    
+  # Optimizer.
+optimizer = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)  # 1e-4
+  
+  # Predictions for the training, validation, and test data.
+train_prediction = tf.nn.softmax(logits)
+valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
+test_prediction = tf.nn.softmax(model(tf_test_dataset))
+
+
+#%%
+num_steps = 1001
+
+with tf.Session(graph=graph) as session:
+  tf.global_variables_initializer().run()
+  print('Initialized')
+  for step in range(num_steps):
+    offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+    batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
+    batch_labels = train_labels[offset:(offset + batch_size), :]
+    feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+    _, l, predictions = session.run([optimizer, cross_entropy, train_prediction], feed_dict=feed_dict)
+    if (step % 50 == 0):
+      print('Minibatch loss at step %d: %f' % (step, l))
+      print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
+      print('Validation accuracy: %.1f%%' % accuracy(
+        valid_prediction.eval(), valid_labels))
+  print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
